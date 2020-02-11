43e1acb7cfb15141d7cd8ad6bd68425469e48a85
lizilong <35825874+liyijieac@users.noreply.github.com>
2020-02-11 16:35:43 +0800

    add  page for amount
    添加页面

--- src/pages/amount/cash/index.vue
+++ src/pages/amount/cash/index.vue
@@ -24,6 +24,19 @@
         <div class="box1">银行卡</div>
       </div>
     </div>
+    <div class="card">
+      <div>卡号</div>
+      <input class="card-input" type="text" placeholder="微信号、支付宝账户、银行卡账户">
+    </div>
+    <div class="name">
+      <div>真实姓名</div>
+      <input class="name-input" type="text" placeholder="请填写真实姓名，以免打错款">
+    </div>
+    <div class="money">
+      <div>提现金额</div>
+      <input class="money-input" type="text" placeholder="可提现金额9999.99">
+    </div>
+    <div class="btn">提交</div>
   </div>
 </template>
 <script src="./index.js"></script>
