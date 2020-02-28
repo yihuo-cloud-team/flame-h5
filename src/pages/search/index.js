b40c12063f950d890e353ed1aa0f0d8b9347167e
sunhui44311 <1874314758@qq.com>
2020-02-28 20:13:32 +0800

    updata

--- src/pages/search/index.js
+++ src/pages/search/index.js
@@ -7,87 +7,115 @@ export default {
   layout: 'sub',
   data() {
     return {
-      value1: 0,
-      value2: 0,
-      zhuangtai: false,
       areaList: areaList,
       option1: [{
-          text: '所有类型',
-          value: 0
-        },
-        {
-          text: '已完成',
-          value: 1
-        },
-        {
-          text: '未完成',
-          value: 2
-        }
+        text: '全部类别',
+        value: ''
+      },
+      {
+        text: '翻译',
+        value: 1
+      },
+      {
+        text: '软件',
+        value: 2
+      },
+      {
+        text: '设计',
+        value: 3
+      },
+      {
+        text: '其他',
+        value: 2
+      }
       ],
       option2: [{
-          text: '所有进度',
-          value: 0
-        },
-        {
-          text: '进行中',
-          value: 1
-        },
-        {
-          text: '已结束',
-          value: 2
-        },
+        text: '所有进度',
+        value: ''
+      },
+      {
+        text: '招募中',
+        value: 0
+      },
+      {
+        text: '进行中',
+        value: 2
+      },
+      {
+        text: '中止',
+        value: 3
+      },
+      {
+        text: '完成',
+        value: 4
+      }
       ],
       list: [],
       loading: false,
       finished: false,
-      keyword: '',
-      addressKeyword: '',
       show: false,
-      Areaval: [],
-      TipsArr: [],
-      areaval: '',
-      addresstitle: '',
-      CurrentAddress: '',
-      location: '',
+      Areaval: '110101',
+      AreaArr: [],
+      district: '朝阳区',
       query: {
-
+        search: '',
+        page: 1,
+        page_size: 10,
+        task_type: '',
+        task_state: '',
+        a: ''
       }
     };
   },
   methods: {
     // 用于初始化一些数据
     init() {
-      this.update();
+      this.getAddress()
     },
     // 用于更新一些数据
     async update() {
-      // AMap.plugin('AMap.Geolocation', () => {
-      //   var geolocation = new AMap.Geolocation({
-      //     enableHighAccuracy: true, //是否使用高精度定位，默认:true
-      //     timeout: 10000, //超过10秒后停止定位，默认：5s
-      //   });
-      //   map.addControl(geolocation);
-      //   geolocation.getCurrentPosition((status, result) => {
-      //       console.log(result)
-      //     if (status == 'complete') {
-          
-      //       this.CurrentAddress = result.addressComponent;
-      //       this.addressKeyword = result.addressComponent.street + result.addressComponent.streetNumber;
-      //       this.location = `${result.position.lng},${result.position.lat}`;
-      //       this.areaval = result.addressComponent.adcode;
-      //       this.addresstitle = result.addressComponent.province;
-      //     } else {
+      try {
+        let res = await this.$http.post('/task/list', this.query)
+        if (res.code > 0) {
+          this.list = res.data
+          if (res.total < 10) {
+            this.finished = true;
+          }
+        } else {
+          this.finished = true;
+        }
+      } catch (error) {
 
-      //     }
-      //   });
-
-      // });
+      }
     },
-    ok(){
-
+    getAddress() {
+      let mapObj = new AMap.Map('iCenter');
+      mapObj.plugin('AMap.Geolocation', () => {
+        let geolocation = new AMap.Geolocation({
+          enableHighAccuracy: true,//是否使用高精度定位，默认:true
+        });
+        geolocation.getCurrentPosition((status, result) => {
+          if (status == 'complete') {
+            this.Areaval = result.addressComponent.adcode
+            this.query.a = result.addressComponent.adcode
+            this.district = result.addressComponent.district
+            this.update();
+          } else {
+            console.warn(status, '定位失败');
+            this.update();
+          }
+        });
+      })
     },
-    quxiao(){
-
+    confirm(e) {
+      this.AreaArr = e
+      this.district = e[2].name
+      this.query.a = e[2].code
+      this.update()
+      this.show = false
+    },
+    cancel() {
+      this.show = false
     },
     async onLoad() {
 
@@ -98,28 +126,28 @@ export default {
   // 包含 Vue 实例可用过滤器的哈希表。
   filters: {},
   // 在实例创建完成后被立即调用
-  created() {},
+  created() { },
   // 在挂载开始之前被调用：相关的 render 函数首次被调用。
-  beforeMount() {},
+  beforeMount() { },
   // el 被新创建的 vm.el 替换，并挂载到实例上去之后调用该钩子。
   mounted() {
- 
+
     this.$nextTick(() => {
-        this.init();
+      this.init();
     });
   },
   // 数据更新时调用，发生在虚拟 DOM 打补丁之前。
-  beforeUpdate() {},
+  beforeUpdate() { },
   // keep-alive 组件激活时调用。
-  activated() {},
+  activated() { },
   // keep-alive 组件停用时调用。
-  deactivated() {},
+  deactivated() { },
   // 实例销毁之前调用。在这一步，实例仍然完全可用。
-  beforeDestroy() {},
+  beforeDestroy() { },
   //Vue 实例销毁后调用。
-  destroyed() {},
+  destroyed() { },
   // 当捕获一个来自子孙组件的错误时被调用。
-  errorCaptured() {},
+  errorCaptured() { },
   // 包含 Vue 实例可用指令的哈希表。
   directives: {},
   // 一个对象，键是需要观察的表达式，值是对应回调函数。
