69a42691713a81d41679a7ccbc1e204611e6825e
代码狮 <1173197065@qq.com>
2020-05-08 14:17:58 +0800

    更改测试人员

--- src/middleware/jwt.js
+++ src/middleware/jwt.js
@@ -1,32 +1,38 @@
+// https://h5.flame.yihuo-cloud.com/user/userInfo?id=49
+
 export default
     {
         jwt:
-            'eyJpdiI6ImJsU3JPT2RmQzNMWHcwaXhMVUVDemc9PSIsInZhbHVlIjoia2x4dzY2R042MzhraVJ3aVFTMWgrV3pQSVZ1ZUx5dzdZUzIrRXlFUEJsSHpSbFAya3pTdGFTM3JEZnhlK0I1WlR0UmM5bnJacVd4R0VjMUZIVGxqWlwvWHJpb01kWlpuajhtNkJtcUk2Z0hVODFZQThYdlk2QmhlUGUxM3Y3cVZxSGZLa0NGTHFkcTBvclB5YlpBSzhkMzlEU0cwMW0zWnZXeE1wNFNXTnZnSE5vcElpWlpcL3BRSXdOKzk0dDFucVwvczZIeVM5UEtUOXRiZjd6bno4endsbU1rZkRvWWdYaUR0ZGpEa210NGF1WE1JTmQ3WUp4aXdvNThlSVZLUGd4ZnlPdWpyWnRlVUd4ZkZyZUVhOW1nQWVFK1wvaFBIYnpyMktZZ1FmTm5DZVpQeGRKYWoxSmdsczlOdlwvRU9DTjFQWVUzKzVQMHBjeVhVaGN3MXIrVmlhdFFFdGYwK3p6a0psc1Fkb21uejh3bkU4bElHbzFFWlVcL0xJR3h5dEhmclwvcWlmNHRRS1dTRW5TdEF5a3gwR1BNMG9TQTRYU0VwTVVsZWV1aVVwV3R0NmZSbGgzaHRPYUxZWUxIUFJYdEIrY283WjU0d01jUzVrcVB1UFV1emhZOUtMMzJROVpPR3grT2ZqTmV4akdXNDRia0ZXaGx5Rlg1OVVlTHU4dHJvR1FwZHhEXC9VanBiYlRNaFZ1Q05VVHQ5XC8zTXVPN3g1eUF3T0VveTJVZlZOMlYwdmgwdjVFOHMwdWRvOTFTXC9HdUxDOE1EY29zUDE5MFJYbnFWNzVibU10bmExNlwvOEx0ZzV4ZHN6aEpLcDdncGxFMzNpblpCSmgya20xaGxyaTBqVUpoVTZuOFQrcnNFUjhpejFseWxEVUYrR01iajYxb3pzNmcwXC83bGpNdWxLWjNwRDUzbkY4amZcL0dybjU5YTZzdGxNM0gzNUU1ZnlwaldPcEw0cDVjZUdQSlNlM2lqTnlZRVZXdlY0OW9YTmI2Y0owUzVjZ1ZnNEFCUm1sOFwvV0hySnZuU1dPR09aTE5SSFdlc0xOVGh0YzN6K24xQUxaaTlcL1ZoUHJUb2h1bldqblNNQUJHemRtUHlkTmJJVm9LQ08yT3hGVDdwdU1QN2JTTUxDYlY4ZFZGSjJzXC9zZE1zSUlYM3k5SDVjZnZBWXZCd2VDWEE4XC9aWDF3RjNcL2VpT2h5Nk1NeW1ObDdlcFZyMnF1V0RmVkdkWkxrazYyVDV1dzhrcm5Td0F5WHhJVFRcL1NuSEVpK2tWVGlTSXl3OTV6dlpFOGIzUTZqbXgyM0UwK2JBaTE2a3dsMDFlSFhDOEh5eEdmTWZMZWxuQUQ0RjUyTTZySnYrRXpEOU5JbCtqbE1MZVVXVGZSNHVwY0xTMW1lYU5CZU9BTVlsS0pOeSs4OHpIbGhPT00zeTBWb1FDRm9oWFhZRXlBZXptejlnZk0yRW85RmdRYXcwb3VCK1IwazhUQ2ZGeU9ha0s4dCt2SFwvVWVORm5XYWNlVTBvODdaNEk3ZFpvcWNDeTBaV0xiTGhUWVNucnZBdkZ4UnBQbUdidlN0aWlUclZ6WUN2bXM5RG4zVEJNdDVJY3NDSkloYlh3Qnk5Sk1BUGtDcG1CeGFncVBlVEVcL0hxS3diTDA0UEI1MUxXRE5Wd3BWcDdMRWY2Z29xc054REpkbGpjYXhkV3BSODR1c0FvNXdmXC9HVzR1bjhqTXBmNllrWTF0RXFDbGs3Y1RNRXNDdTR4dllBZUsxUllUQkZHOE4zZTBaMEdISm1BTFNSd3Z0cHhua0RXeDl3K1pLaFE1dE93bVliT1A5aENCckVrdmxUUmEySXVQOHlRa2lqNkk1bTFTWGtVUm5Wa2ZaTDc0b2RZYTNqcys2Q253MEl1YUJjazcyWHJtUGJjN3k4QVZDQ0VrdzhoTVR3emhjcUNGQ282ME9nPSIsIm1hYyI6ImVhYzFmODMwZTNkYWNkYWE1OWE3OGQ2YzNlNzAwNGNjYThjNzgwZjg4Nzk1ZDNjNDZlN2U1NzllMzJjNDQwM2EifQ==',
-        userInfo: {
-            add_time: "2020-03-02 12:02:25",
-            birthday: "1950年01月1日",
-            birthday_show: 0,
-            data_state: 1,
-            edit_time: "2020-03-21 17:50:25",
-            freeze_money: "0.00",
-            gender: 1,
-            gender_show: 0,
-            head_img: "http://thirdwx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTKNR7sUQ5LdvicPVSRhojDuSPicI5p4v23SMfNnjmicibH64PgAT2S2iaDgsFtbqaRYautsm1zseic92tzQ/132",
-            id: 44,
-            name: "惠普",
-            name_show: 0,
-            openid: "oCIrqvkpXqMkEe3jTfTkodpsD2OY",
-            phone: "18903030403",
-            phone_show: 0,
-            power_group_id: -1,
-            pwd: null,
-            real_name: null,
-            skill: "e t",
-            skill_show: 0,
-            unionId: "oMJGss5sA3K6ZaBjCIU-fzU_002s",
-            user_money: "0.00",
-            user_type: 2,
-            wx_info: { "openid": "oCIrqvkpXqMkEe3jTfTkodpsD2OY", "nickname": "PerfectMan Sun", "sex": 1, "language": "zh_CN", "city": "\u677e\u6c5f", "province": "\u4e0a\u6d77", "country": "\u4e2d\u56fd", "headimgurl": "http: \/\/ thirdwx.qlogo.cn\/mmopen\/vi_32\/Q0j4TwGTfTKNR7sUQ5LdvicPVSRhojDuSPicI5p4v23SMfNnjmicibH64PgAT2S2iaDgsFtbqaRYautsm1zseic92tzQ\/132", "privilege": [], "unionid": "oMJGss5sA3K6ZaBjCIU-fzU_002s" },
+            'eyJpdiI6InpIdSt3bjdZejMzcFwvemtZUXRoQUNBPT0iLCJ2YWx1ZSI6ImVkS0RlNzFQTHArQVJLdjd4OWhGXC9vUStHU0lBZXg2Qm12bnh3UjU1b1J0SFRkWjdlaDIrcnF4YTZmam5kRnM5MDdNUzRnOTUwNnNlbXdIcmNsZG50cE85cjVvMUxwZ1R1bGlYZjY2TVQ2K3lCQU5SbTNBSXlmaktmRnZmWHpUamQ3SEcyQ2piV0FoMEp0clRQbm9zV05aK3djZmc1M29NaVZXY3lJcmlmbzdLVkVqcXdPR2dzVjVENEhpNkRTaEhGN2VxeWZxNEdFRG1BWG84QlBSQ25mK1ZCZWRxa3NEUkxOd2g1Nlc0SU91MlN5Ynl4Z3RtaE84Q1Nvd3NPdGJuMHBjbDdnelhXXC9KNGt0WDJiQjA1UmZxeEhWdE93YlYrY2F5aVhjTzU4TndvOHpZdU9WZlBNVStTVnA3T3RrNXJUMGQ1c3pwdWVDeUliOTJmN2hmTCtldEVheUR6MlZSZ3Y0NGRZYjczSUtGejhPNDhYVW15S1UrMFA3enNtRTJMUjBFRnRBcUNMb3dMY1BXZTJDWmJNSGNQYUFvMjZ3QWJaYXIraENTWFlDTENPUnlsNTlQQllDbDFudnl2N0tITEdtM2ZZXC9YK3JwdWoyS3JEYmlaM0hKb2s3dkRKb3hhdFNzcWhJT1RpUnlvczl0MzJuTXlMWDdMZHFJNjJoZWR5Y3hoMkVXSE1iR0RVWFdzdnV0QzJyMTJ1RUYycXArTTlUSkJNK1BCbTVnQVdCZzRwQlJ3bU9RUHdkamdNNFhEcnpsY3dOYXYrNm1TS0ZIWDJUeXZUXC9oUCs5ZEU0MEYzRXlLNXVQbHI1MEl5cnBicU1qSXFLaEg2XC84YmsyUmJaeFMweTNnaFgxcXZxeGdoQWN3Y1pMT0ZSdVRQazNubFQ1ODJjcVNyazNJSWtXVGdYUExoc3AydjE4Smg5MmpwbzFZQ3ZodjlxdjRiZVFwSk45MW1yQ1ZEWHpXcjE4NXR0d01ldHpjc2JyZHd5dHd2a0lKdnI2XC9ISkN0OW9FTTBpV0RORmJUXC92dktTWCttTXFlS1c1bEdiMXpJbE0rOTQ5NUZpWDNWdEF0K1RyOEswZ2lQMmdpcUltRWtycTh4Rzhod0QydGFucHlaRVlEUzBkYUVlYndtNTQ1NU5TTkJIWGZDUkUycjYxYllDaGNYcG9qaTNKMjR5c3FJcklKZ3N3XC8xRGNUaHRObWw0VVljaVdUdVJJb2xWZG1BRkJSSmRrQVwvRnV3RTFOMXVJM2VqWG9waFhqS21cL2kzQVJVaGpnbU9Fdzl4NVpIS3k1eDArc2lwVzUwNndwWGVZUjd0Tk9XVzZ6ejZHa1I4c2hQSE45RE1tZnE4WlN0b3hcL0NuMmlSQlkyMzNXRzEyb0o1WndaOEV2WjZGSkJKN3hieUhOeXJFMXVKZWpzeFVUR0JzTEJ0VmU1R3hUTjc3NFc0eWw1ZkJuQUo4UW1yMVN0dGlNQjNlTkhGb1BYU3JwT2RFTEtHV3Y4Z1BoY29wWjA2SGtyRURMQXVIRFhEb0d1aW5qUnRBQTY3bTkwalZ5ejRXdU8wdWFOSXM1OEdhXC9qeEk2ZE5DWk9ZWlhqdjZwMUhWc1hxbWRPbmpGXC96RG1JNjh1YVJ5blpzODZIaU1peFVCXC9ZS3MxaG9qcCtlaWkwbUdaRnJWdkl4emxSVVNEWnZBakc2cVpOckhNMlhQcG1rckxJbjJYVVNZSGFBbDdnTndZUzg0ZzVRUk1JRVwvNFdrWThJbzFueVlyQzl3TkZuTEd5SGdNMFwvRkM1cktuZnQzUXNZM3hHMjJkOU1JeEZ1Z3JqUlFkNFwvRHZmTHZsUmVSM1o5SE12Y0dJT2hXdkVMUlFWYnRibWRNQXRLY1NDVTRxaTMzbk5WbjRjandmWHdzZXZRcUtxamlTQmJQT0tvVFpNZkRDMWxvaUlxTGFBTkFvbnEzYU1PallHTmlqWHRUOGtuaUV2Rm5JMTRJKzJWc3JuQXZoQW8xQVFtcGFaRE5EbjBDSzV4QzM5WmwwYkVXMjZVXC9DZ0t2QXRsMDllTzBKWkxSNDUzc2dJRG52UmxOK2RcLzUxUW0zME1kRVA5enp2YklhQnZjMncrWUdKaUV6TkRCSVJuWXJyZTlkcENtQTArbGJydktxR1AzUFlORjZSd296Ukt6V1lTdjVubVpKaHk5V1Q1Y2t0Y1QxdHZwbWZzUnJkR0x1amVGQVVwTFVLZldkRGVWR3NcLytLVWdcLzB3V1BnendrSzJnYXpwOGdHTkFabVpmYkJKRUx3ZlFiM09IWWJVSWdzVzJDQncyc0hBMWhIV3lLTDAxVVUyaHZ0ZGN6MlVJQXBWV3NDTVBiOEQ3ekZUNVpGSnAzT2RERVdlaDIzdnhUZHcwRENFWGFiVDY0MmQrcnlaNmFRTkdaRURJdjhnOGg3VmJNelwvTDFjaGtaUzZGejRzSWxIa0dvYlhTU3IrVUxBdGtXREtTc0JkQ0p3eGN0SHJ1V0xFMG9TS2dJbytIS0xWMGVUclFBUmRoSGtwMVpiRWZpY2JoMHpaTzg4YUZRQ0QwZURnNHI2OEtLR05MbXUrWUZ0cWZWa29CMjlaNGkxd3pETFRaMXNUcjIzWWFMTHlBQTFzM0JyTkdzWXdVY0FEUmJtTG1RQnkzcGhHZUoyWENjUzIrOUNzMnNnXC8xZlwvK0hndHBUcE4rVjZcL2JRZFFvalBUSWlLb3pXSVRTZ0puYnl1c0gxaW5KU3gxU1gwbDNlQ25idVZMNDVIbnBsZ3dXV1BxSFRZeElUTlBLd0gwXC92dlZPVHE0YlU1VEpLcWtvdm5oMFZkVEJrWU5ZIiwibWFjIjoiOWE3MDFkNGIwYTUxNDBmM2UxZjJmMGJmMzhlNmNjMDllYzg0MzQ5NzU5ZGI1ZjU3MzgyY2U3OTcwZTkxNWViNSJ9',
+        userInfo:
+        {
+            "id": 49,
+            "real_name": null,
+            "openid": "oCIrqvvPxQbOD0igzc8NTIDZElSA",
+            "unionId": "oMJGss-h51fMChWBZZlLyBNNf050",
+            "phone": "thomas-512",
+            "phone_show": 1,
+            "wx_info": "{\"openid\":\"oCIrqvvPxQbOD0igzc8NTIDZElSA\",\"nickname\":\"\\u6258\\u9a6c\\u65af\",\"sex\":1,\"language\":\"zh_CN\",\"city\":\"\\u677e\\u6c5f\",\"province\":\"\\u4e0a\\u6d77\",\"country\":\"\\u4e2d\\u56fd\",\"headimgurl\":\"http:\\\/\\\/thirdwx.qlogo.cn\\\/mmopen\\\/vi_32\\\/DYAIOgq83ersr0GibD46VicxCvPRym8MibH3YnhlcZrTeHylrqPz8LA5Y5TD4zoM2bzmYVibhqrHDf6MDNG72vjPKw\\\/132\",\"privilege\":[],\"unionid\":\"oMJGss-h51fMChWBZZlLyBNNf050\"}",
+            "gender": 1,
+            "gender_show": 0,
+            "name": "\u6258\u9a6c\u65af",
+            "name_show": 1,
+            "birthday": "1987\u5e7410\u67086\u65e5",
+            "birthday_show": 0,
+            "skill": "IT",
+            "skill_show": 1,
+            "head_img": "http:\/\/thirdwx.qlogo.cn\/mmopen\/vi_32\/DYAIOgq83ersr0GibD46VicxCvPRym8MibH3YnhlcZrTeHylrqPz8LA5Y5TD4zoM2bzmYVibhqrHDf6MDNG72vjPKw\/132",
+            "user_type": 2,
+            "power_group_id": -1,
+            "pwd": null,
+            "freeze_money": "0.00",
+            "user_money": "0.00",
+            "like_list": "[{\"color\":\"#F56C6C\",\"text\":\"\\u770b\\u7535\\u5f71\"},{\"color\":\"#67C23A\",\"text\":\"\\u770b\\u7f8e\\u5267\"},{\"color\":\"#909399\",\"text\":\"\\u7f8e\\u98df\"}]",
+            "img_list": "[\"\\\/public\\\/files\\\/20200507\\\/202005070946256690.jpeg\",\"\\\/public\\\/files\\\/20200508\\\/202005081236469631.jpeg\",\"\\\/public\\\/files\\\/20200508\\\/202005081239378714.jpeg\",\"\\\/public\\\/files\\\/20200508\\\/202005081241515639.jpeg\"]",
+            "constellation": "\u5929\u79e4\u5ea7",
+            "add_time": "2020-03-06 18:11:32",
+            "edit_time": "2020-05-08 12:41:52",
+            "data_state": 1
         }
 
     }
