c201a946f5ed0c84f44f2dcad873629eb2f1729f
代码狮 <1173197065@qq.com>
2020-05-15 19:41:29 +0800

    yasgh

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -4,7 +4,7 @@ import jwt from './jwt'
 export default function (context) {
     localStorage.jwt = jwt.jwt;
 
-    let v = '8';
+    let v = '9';
 
     if (localStorage.version_nb != v) {
         localStorage.version_nb = v;
