98ddd5db7f0615a5fa5467237e65a48587814035
sunhui44311 <1874314758@qq.com>
2020-05-15 18:23:53 +0800

    updata

--- src/pages/enroll/index.js
+++ src/pages/enroll/index.js
@@ -3,8 +3,8 @@ export default {
   layout: 'sub',
   data() {
     return {
-      form:{
-        display:0,
+      form: {
+        display: 0,
         message: ''
       }
     };
@@ -19,12 +19,12 @@ export default {
 
     },
     async submit() {
-      if (this.message == '') {
-          this.$toast('请简述原因');
+      if (this.form.message == '') {
+        this.$toast('请简述原因');
         return false
       }
       const res = await this.$http.post('/task/apply', {
-        text: this.message,
+        text: this.form.message,
         task_id: this.$route.query.id
       });
       if (res.code >= 0) {
@@ -33,7 +33,7 @@ export default {
       } else {
         this.$toast(res.msg);
       }
-  
+
     }
   },
   // 计算属性
@@ -41,26 +41,26 @@ export default {
   // 包含 Vue 实例可用过滤器的哈希表。
   filters: {},
   // 在实例创建完成后被立即调用
-  created() {},
+  created() { },
   // 在挂载开始之前被调用：相关的 render 函数首次被调用。
-  beforeMount() {},
+  beforeMount() { },
   // el 被新创建的 vm.el 替换，并挂载到实例上去之后调用该钩子。
   mounted() {
     this.init();
-    this.$nextTick(() => {});
+    this.$nextTick(() => { });
   },
   // 数据更新时调用，发生在虚拟 DOM 打补丁之前。
-  beforeUpdate() {},
+  beforeUpdate() { },
   // keep-alive 组件激活时调用。
-  activated() {},
+  activated() { },
   // keep-alive 组件停用时调用。
-  deactivated() {},
+  deactivated() { },
   // 实例销毁之前调用。在这一步，实例仍然完全可用。
-  beforeDestroy() {},
+  beforeDestroy() { },
   //Vue 实例销毁后调用。
-  destroyed() {},
+  destroyed() { },
   // 当捕获一个来自子孙组件的错误时被调用。
-  errorCaptured() {},
+  errorCaptured() { },
   // 包含 Vue 实例可用指令的哈希表。
   directives: {},
   // 一个对象，键是需要观察的表达式，值是对应回调函数。
